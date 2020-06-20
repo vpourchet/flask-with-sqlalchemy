b260cbdff4625ea4dd323d1b80c806a4810d588d
Valentin Pourchet <valentin.pourchet@gmail.com>
2020-06-20 11:37:14 +0200

    Fixed the app

--- wsgi.py
+++ wsgi.py
@@ -1,39 +1,26 @@
-# wsgi.py
-import os
-import logging
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_restx import Namespace, Resource, fields
-
-api =  Namespace('Products')
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
-logging.warn(os.environ["DUMMY"])
+from schemas import product_schema, products_schema
 
-@api.route('/hello')
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return "Hello world!"
 
-@api.route('/products')
+@app.route('/products')
 def products():
-    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    products = db.session.query(Product).all()
     return products_schema.jsonify(products)
 
-@api.route('/products/<int:id>')
-@api.response(404, 'Product not found')
-@api.param('id', 'The product unique identifier')
-def get(self, id):
-        product = db.session.query(Product).get(id)
-        if product is None:
-            api.abort(404, "Product {} doesn't exist".format(id))
-        else:
-            return product
+@app.route('/products/<int:id>')
+def product(id):
+    product = db.session.query(Product).get(id)
+    return product_schema.jsonify(product)
