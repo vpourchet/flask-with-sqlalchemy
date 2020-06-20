50514b2bb551817837492a3796e53235664fd943
Valentin Pourchet <valentin.pourchet@gmail.com>
2020-06-20 11:33:27 +0200

    WIP

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 # wsgi.py
+import os
+import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_restx import Namespace, Resource, fields
+
+api =  Namespace('Products')
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,13 +17,23 @@ ma = Marshmallow(app)
 
 from models import Product
 from schemas import products_schema
-#logging.warn(os.environ["DUMMY"])
+logging.warn(os.environ["DUMMY"])
 
-@app.route('/hello')
+@api.route('/hello')
 def hello():
     return "Hello World!"
 
-@app.route('/products')
+@api.route('/products')
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@api.route('/products/<int:id>')
+@api.response(404, 'Product not found')
+@api.param('id', 'The product unique identifier')
+def get(self, id):
+        product = db.session.query(Product).get(id)
+        if product is None:
+            api.abort(404, "Product {} doesn't exist".format(id))
+        else:
+            return product
